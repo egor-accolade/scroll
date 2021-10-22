6658b1d8030a96de2f1408ce51c2bf918a907b37
Egor Gromyko <egor.gromyko@accolade.com>
2021-10-22 14:29:30 +0200

    Remove useEffect

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import { Info } from "./Info";
 import { Loading } from "./Loading";
 import { fetchFromApi } from "./fetchFromApi";
@@ -8,11 +8,6 @@ export default function Container() {
 
   // Make a fake "API" call using fetchFromApi()
   // set loading to 'false' when the "request" is done
-
-  useEffect(async () => {
-      await fetchFromApi();
-      setLoading(false);
-  }, [])
 
   return (
     <div className="wrapper">
