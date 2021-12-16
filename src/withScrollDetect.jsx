85dae634dfd276dbc27f13394e3faa2d9bb6cb85
Egor Gromyko <egor.gromyko@accolade.com>
2021-12-16 13:39:00 +0100

    Add task desc for withScrollDetect HOC

--- src/withScrollDetect.jsx
+++ src/withScrollDetect.jsx
@@ -1,3 +1,4 @@
 export const withScrollDetect = (WrappedComponent) => function Component({...props}) {
+  // should pass a scrolled=true prop to WrappedComponent if user scrolled 400px from the top of the screen
   return <WrappedComponent {...props}/>;
 };
